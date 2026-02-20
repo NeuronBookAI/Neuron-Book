21a67a88aa12b32d6f8749644034958dfbc17a46
Matthew Garcia <gmatthew2765@gmail.com>
2026-02-20 02:10:35 -0800

    fixed deployment runtime issue

--- proxy.ts
+++ proxy.ts
@@ -1,5 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
+export const runtime = "edge";
+
 const isProtectedRoute = createRouteMatcher([
   "/dashboard(.*)",
   "/reader(.*)",
